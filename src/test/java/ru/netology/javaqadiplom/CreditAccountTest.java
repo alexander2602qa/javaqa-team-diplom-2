cc366aef896c61060ef3d499c89b5c6f1ab87d00
Alexander <alex.albasov@gmail.com>
2023-05-30 20:20:32 +0700

    added 2 branches

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -2,7 +2,7 @@ package ru.netology.javaqadiplom;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+// комментарий для теста гита
 public class CreditAccountTest {
 
     @Test
